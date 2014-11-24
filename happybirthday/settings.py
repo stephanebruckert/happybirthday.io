279058ed1477bc236c22a9f0b6613b29d1fec79d
Stéphane Bruckert <contact@stephanebruckert.com>
2014-11-24 20:48:17 +0100

    Added timeline app

--- happybirthday/settings.py
+++ happybirthday/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import mongoengine
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -58,10 +59,23 @@ WSGI_APPLICATION = 'happybirthday.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': '',
+    },
 }
+
+_MONGODB_USER = 'admin'
+_MONGODB_PASSWD = 'mypwd'
+_MONGODB_HOST = 'localhost'
+_MONGODB_NAME = 'happybirthday'
+_MONGODB_DATABASE_HOST = \
+    'mongodb://%s:%s@%s/%s' \
+    % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
+
+#mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST)
+
+AUTHENTICATION_BACKENDS = (
+    'mongoengine.django.auth.MongoEngineBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
---
+++ timeline/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'stephane bruckert'
+
+from django.conf.urls import url
+
+from timeline import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
